34e27280a2250e3a5118b18218f1c8a45fea5a56
Михаил Кормановский <kormanowsky@gmail.com>
2020-07-01 01:01:44 +0300

    Added 3 subjects to the website

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import React from "react";
 import Constants from "./constants";
 import "./App.css";
 
+function HomeSubject({ subject }) {
+    return (
+        <div className="block block-home-subject">
+            <a href={subject.url}>
+                <i className={subject.icon}></i>
+                <p>{subject.name}</p>
+            </a>
+        </div>
+    );
+}
+
 function App() {
     return (
         <div className="app app-home">
             <div>
                 <h1>{Constants.projectInfo.name}</h1>
                 <h4>{Constants.projectInfo.description}</h4>
-                <p>Made by <a href="https://vk.com/kormanowsky/" target="_blank" rel="noopener noreferrer">Mikhail Kormanowsky</a> using IcoFont icons and Google Fonts.</p>
+
+                <div id="home-subjects">
+                    {Constants.subjects.map((subject) => (
+                        <HomeSubject subject={subject} />
+                    ))}
+                </div>
+
+                <p>
+                    Made by{" "}
+                    <a
+                        href="https://vk.com/kormanowsky/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Mikhail Kormanowsky
+                    </a>{" "}
+                    using IcoFont icons and Google Fonts.
+                </p>
             </div>
         </div>
     );
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,23 @@
 export default {
     projectInfo: {
-        name: "Project1", 
+        name: "Project1",
         description: "Все о ЕГЭ",
-    }
-}
+    },
+    subjects: [
+        {
+            name: "Русский язык",
+            icon: "icofont-font",
+            url: "/subject/russian/",
+        },
+        {
+            name: "Математика",
+            icon: "icofont-mathematical-alt-1",
+            url: "/subject/maths/",
+        },
+        {
+            name: "Информатика и ИКТ",
+            icon: "icofont-keyboard-alt",
+            url: "/subject/informatics/",
+        },
+    ],
+};
