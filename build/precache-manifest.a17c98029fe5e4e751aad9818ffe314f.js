1e217af8dea5f2b5fbaa6d1e1fa841d9fe60959b
Михаил Кормановский <kormanowsky@gmail.com>
2020-07-14 21:44:50 +0300

    Added /build to the repo to serve it on Github

---
+++ build/precache-manifest.a17c98029fe5e4e751aad9818ffe314f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce17ae94ef7304db4dc0049b9b54acd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ac81953bfc8c968416",
+    "url": "/static/css/main.bdaeef97.chunk.css"
+  },
+  {
+    "revision": "c17d15bee674e0532e4a",
+    "url": "/static/js/2.ea1842f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ea1842f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ac81953bfc8c968416",
+    "url": "/static/js/main.0df3c02f.chunk.js"
+  },
+  {
+    "revision": "e3b2980271a8fb761485",
+    "url": "/static/js/runtime-main.e090e440.js"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/static/media/icofont.f6ab04ae.woff"
+  }
+]);
