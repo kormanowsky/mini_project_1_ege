de298fb792067b736412e9e012f23e77af30781b
Михаил Кормановский <kormanowsky@gmail.com>
2020-07-14 22:41:43 +0300

    Updates

---
+++ precache-manifest.43f8a7a682d37dc3564ad70e948c90e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b954bd1ba41627c0a9eae1aab0371d62",
+    "url": "/mini_project_1_ege/index.html"
+  },
+  {
+    "revision": "f841241129e5fba0d7d1",
+    "url": "/mini_project_1_ege/static/css/main.84128d96.chunk.css"
+  },
+  {
+    "revision": "c17d15bee674e0532e4a",
+    "url": "/mini_project_1_ege/static/js/2.ea1842f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mini_project_1_ege/static/js/2.ea1842f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f841241129e5fba0d7d1",
+    "url": "/mini_project_1_ege/static/js/main.01a0f791.chunk.js"
+  },
+  {
+    "revision": "b572f484720b195f2988",
+    "url": "/mini_project_1_ege/static/js/runtime-main.cf08965f.js"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/mini_project_1_ege/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/mini_project_1_ege/static/media/icofont.f6ab04ae.woff"
+  }
+]);
